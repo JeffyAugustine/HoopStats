c71c6a0582bf84ad3bfd70e69f99a353044ce19a
JeffyAugustine <jeffy22@umd.edu>
2025-09-23 18:03:35 +0000

    started 1st notebook , prepared config files, helpers and functions scripts . prepared frame view.

---
+++ project_config.py
@@ -0,0 +1,12 @@
+DATA_PATH = 'HoopStats_assets/data'
+
+# Encoder settings
+ENCODER_PATH = "HoopStats_assets/models/mars/mars-small128.pb"
+ENCODER_BATCH_SIZE = 32
+ENCODER_INPUT_NAME = "images"
+ENCODER_OUTPUT_NAME = "features"
+
+# Object detection model
+MODEL_PATH = 'HoopStats_assets/models/frcnn'
+
+
